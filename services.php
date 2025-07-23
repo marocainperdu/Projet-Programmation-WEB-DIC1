06800edba10d68201de3582676ec7cb1aef725d8
ndeye532 <ndeyegueye81004@gmail.com>
2025-07-23 16:04:28 +0200

    Create services.php

---
+++ services.php
@@ -0,0 +1,13 @@
+<?php include("header.php"); ?>
+
+<div class="contenu">
+    <h1>Nos Services</h1>
+
+    <ul>
+        <li>Aménagement d'intérieur personnalisé</li>
+        <li>Décoration événementielle (mariage, soirée)</li>
+        <li>Conseils décoration à distance</li>
+    </ul>
+</div>
+
+<?php include("footer.php"); ?>
